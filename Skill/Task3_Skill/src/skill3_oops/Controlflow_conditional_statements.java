567a29d053312cb1f5267770b31fc6053d1bfc8a
kridhay <2420030211@klh.edu.in>
2025-08-02 10:32:41 +0530

    Skill Tasks

---
+++ Skill/Task3_Skill/src/skill3_oops/Controlflow_conditional_statements.java
@@ -0,0 +1,19 @@
+package skill3_oops;
+import java.util.Scanner;
+class Controlflow_conditional_statements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+System.out.println("enter your age");
+int age=sc.nextInt();
+if(age>18)
+	System.out.println("adult, eligible to vote");
+else
+	System.out.println("not adult,not eligible to vote");
+
+	
+
+	}
+
+}
